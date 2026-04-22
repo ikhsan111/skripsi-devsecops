e014c8bb1a186cce775d39b33f762f35e791c522
ikhsan111 <ikhsanfadillah051003@gmail.com>
2026-04-22 18:43:09 +0700

    feat: memancing push dari vs code

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask
-import hashlib
 
 app = Flask(__name__)
 
+# =====================================================================
+# SKENARIO 3: Mengaktifkan Mode Debug secara Global
+# Penulisan seperti ini pasti akan memicu alarm Security Hotspot
+# Memancing Git agar mau push.
+# =====================================================================
+app.config['DEBUG'] = True
+app.debug = True
+
 @app.route('/')
 def index():
-    # =====================================================================
-    # SKENARIO 3 (BARU): Security Misconfiguration (Weak Cryptography)
-    # Mengonfigurasi sistem untuk menggunakan algoritma MD5 yang sudah usang.
-    # Ini PASTI akan memicu alarm Security Hotspot dari SonarQube.
-    # =====================================================================
-    data_rahasia = "password_pengguna_123"
-    konfigurasi_hash_lemah = hashlib.md5(data_rahasia.encode()).hexdigest()
-    
-    return f"Sistem berjalan. Hash MD5: {konfigurasi_hash_lemah}"
+    return "Menguji Skenario 3: Debug Mode"
