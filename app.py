09e133020464c82d925f6f5c23dc27f12873a151
ikhsan111 <ikhsanfadillah051003@gmail.com>
2026-04-22 19:05:15 +0700

    Test Otomatisasi Poll SCM

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # =====================================================================
 # SKENARIO 3: Mengaktifkan Mode Debug secara Global
 # Penulisan seperti ini pasti akan memicu alarm Security Hotspot
-# Memancing Git agar mau push
+# Memancing Git agar mau push . .
 # =====================================================================
 app.config['DEBUG'] = True
 app.debug = True
