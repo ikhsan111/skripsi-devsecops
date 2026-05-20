193afb243417ef26accd7965dde477c3b3f666b2
ikhsan111 <ikhsanfadillah051003@gmail.com>
2026-05-20 22:27:04 +0700

    5

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import request
+from flask import Flask, request, send_from_directory
 
-# --- SKENARIO CWE-22 (Path Traversal) ---
-# SonarQube akan mendeteksi ini sebagai Vulnerability (Critical)
-@app.route('/read')
-def read_file():
-    filename = request.args.get('file')
-    # Penyerang bisa melakukan traversal: ?file=../../etc/passwd
-    with open(os.path.join('/var/www/uploads', filename), 'r') as f:
-        return f.read()
-# ----------------------------------------
+app = Flask(__name__)
+UPLOAD_FOLDER = '/var/www/uploads'
+
+# --- SKENARIO KERENTANAN: CWE-22 (Path Traversal) ---
+# SonarQube akan mendeteksi fungsi ini sebagai 'Vulnerability' 
+# karena input 'filename' tidak divalidasi dan langsung digunakan untuk akses file.
+@app.route('/download')
+def download_file():
+    filename = request.args.get('filename')
+    # Penyerang bisa mengirim: ?filename=../../../../etc/passwd
+    # SonarQube menangkap pola ini sebagai path traversal yang berbahaya.
+    return send_from_directory(UPLOAD_FOLDER, filename)
+# ----------------------------------------------------
+
+if __name__ == '__main__':
+    app.run()
