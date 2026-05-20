bd404a9bf05fbcd53dbf870e3cd52d783d7b1544
ikhsan111 <ikhsanfadillah051003@gmail.com>
2026-05-20 22:04:53 +0700

    5

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from flask import Flask
+from flask_wtf.csrf import CSRFProtect, csrf
 
-# Rule CSRF SonarQube akan langsung mendeteksi decorator ini
-@csrf_exempt
-def my_view(request):
-    return HttpResponse("CSRF protection is disabled!")
+app = Flask(__name__)
+
+# 1. PELANGGARAN GLOBAL:
+# Menonaktifkan CSRF secara global melalui konfigurasi aplikasi
+app.config['WTF_CSRF_ENABLED'] = False 
+
+csrf = CSRFProtect()
+csrf.init_app(app)
+
+# 2. PELANGGARAN PADA VIEW:
+# Menggunakan @csrf.exempt secara eksplisit menonaktifkan proteksi pada endpoint ini
+@app.route('/transfer', methods=['POST'])
+@csrf.exempt  # SENSITIF: Ini akan langsung memicu rule SonarQube
+def transfer_money():
+    return "Dana telah ditransfer tanpa proteksi CSRF."
+
+if __name__ == '__main__':
+    app.run(debug=True)
