7cfcbbbf69b0d5f03afd5c2a231c711d9bc41f8f
ikhsan111 <ikhsanfadillah051003@gmail.com>
2026-05-20 22:00:11 +0700

    4

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
-from flask import Flask, request
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
-app = Flask(__name__)
-
-# --- SKENARIO KERENTANAN: CWE-352 (CSRF) ---
-# SonarQube akan mendeteksi ini sebagai kerentanan karena melakukan 
-# 'state-changing operation' tanpa validasi anti-CSRF token.
-@app.route('/update-password', methods=['POST'])
-def update_password():
-    # Input dari user diambil langsung tanpa pengecekan token
-    new_password = request.form.get('password')
-    
-    # Simulasi update ke database
-    # Pada titik ini, aplikasi rentan terhadap serangan CSRF.
-    # Penyerang bisa memaksa korban menekan link yang mengirim POST ke sini.
-    return f"Password berhasil diubah menjadi: {new_password}"
-# -------------------------------------------
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Rule CSRF SonarQube akan langsung mendeteksi decorator ini
+@csrf_exempt
+def my_view(request):
+    return HttpResponse("CSRF protection is disabled!")
